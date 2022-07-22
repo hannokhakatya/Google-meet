68388df49300bb8210b3a9663dc7ba06648800b1
hannokhakatya <91082973+hannokhakatya@users.noreply.github.com>
2022-07-22 15:20:04 +0300

    create store and add photo

---
+++ client/src/store.js
@@ -0,0 +1,32 @@
+import Kate from '../src/photos/Kate.jpeg';
+import Anton from '../src/photos/Anton.jpeg';
+import Bob from '../src/photos/Bob.jpeg';
+import Dasha from '../src/photos/Dasha.jpeg';
+import David from '../src/photos/David.jpeg';
+import Kleo from '../src/photos/Kleo.jpeg';
+import Lena from '../src/photos/Lena.jpeg';
+import Lili from '../src/photos/Lili.jpeg';
+import Nastya from '../src/photos/Nastya.jpeg';
+import Nick from '../src/photos/Nick.jpeg';
+import Oleh from '../src/photos/Oleh.jpeg';
+import Viola from '../src/photos/Viola.jpeg';
+
+let store = {
+        users: [
+            {id:1, name:'Kate' , userPhoto: Kate},
+            {id:2, name:'Anton' , userPhoto: Anton},
+            {id:3, name:'Bob' , userPhoto: Bob},
+            {id:4, name:'Dasha' , userPhoto: Dasha},
+            {id:5, name:'David' , userPhoto: David},
+            {id:6, name:'Kleo' , userPhoto: Kleo},
+            {id:7, name:'Lena' , userPhoto: Lena},
+            {id:8, name:'Lili' , userPhoto: Lili},
+            {id:9, name:'Nastya' , userPhoto: Nastya},
+            {id:10, name:'Nick' , userPhoto: Nick},
+            {id:11, name:'Oleh' , userPhoto: Oleh},
+            {id:12, name:'Viola' , userPhoto: Viola},
+        
+        ]
+};
+
+export default store;
